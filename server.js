2e6d8ecbcd73627c68d1bf3763d744d1e0266d14
ikamean <ika.bidzishvili@gmail.com>
2021-05-17 12:51:59 +0400

    cors methods fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const loginRouter = require('./routes/login.js');
 app.use(express.json());
 app.use(cors({
     origin: '*',
-    methods: [ GET, POST, PUT, DELETE ]
+    methods: [ "GET", "POST", "PUT", "DELETE" ]
 }));
 
 
