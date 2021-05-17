9769ad12e3cb8a46b83cd32a9af6f31c236d5bf0
ikamean <ika.bidzishvili@gmail.com>
2021-05-17 12:58:08 +0400

    PORT fix

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', ( req , res ) => {
 
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is listening to port ${PORT}`));
 
