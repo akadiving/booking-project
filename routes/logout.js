4d2d6aa468dc1cc6956a0b0ffb629f527ad95902
ikamean <ika.bidzishvili@gmail.com>
2021-05-26 12:11:57 +0400

    loges

--- routes/logout.js
+++ routes/logout.js
@@ -22,8 +22,12 @@ router.post('/', async (req, res) => {
             Removing Cookie in client browser. 
             */
 
+    console.log('session', req.session);
+
     req.session.cookie.expires = new Date().getTime();
     let cookieKey = req.headers.cookie.split('=')[2];
+    console.log('splitted cookie array =>>>', req.headers.cookie.split('='));
+    console.log('cookie key =>>>>', cookieKey);
     res.clearCookie(cookieKey);
 
     res.sendStatus(200);
